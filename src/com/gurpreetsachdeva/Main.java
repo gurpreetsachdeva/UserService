f2c5f662c03a0fc7c3d10f5bfb78a5308f5c697c
Gurpreet Sachdeva <gurpreet.sachdeva@turtlemint.com>
2020-04-24 10:40:30 +0530

    Initial Commit

---
+++ src/com/gurpreetsachdeva/Main.java
@@ -0,0 +1,12 @@
+package com.gurpreetsachdeva;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        User test=new User();
+        System.out.println("Exiting user");
+
+    }
+}
---
+++ src/com/gurpreetsachdeva/User.java
@@ -0,0 +1,9 @@
+package com.gurpreetsachdeva;
+
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
